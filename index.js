dd05a50c7a5985319e251a28381d40163debf7d0
degradik <66140168+degradik@users.noreply.github.com>
2024-04-13 21:58:46 +0300

    +completed, inprocces json files +routes

--- index.js
+++ index.js
@@ -9,18 +9,31 @@ app.get('/', (req, res) => {
     res.send('Привет, мир!');
 });
 
-app.get('/get', (req, res) => {
+app.get('/get/completed', (req, res) => {
     // Читаем файл example.json
-    fs.readFile('example.json', 'utf8', (err, data) => {
-      if (err) {
+    fs.readFile('json/completed.json', 'utf8', (err, data) => {
+        if (err) {
         console.error('Ошибка чтения файла:', err);
         res.status(500).send('Произошла ошибка на сервере');
         return;
-      }
-      // Отправляем содержимое файла как JSON
-      res.json(JSON.parse(data));
+        }
+        // Отправляем содержимое файла как JSON
+        res.json(JSON.parse(data));
     });
-  });
+});
+
+app.get('/get/inprocces', (req, res) => {
+    // Читаем файл example.json
+    fs.readFile('json/inprocces.json', 'utf8', (err, data) => {
+        if (err) {
+        console.error('Ошибка чтения файла:', err);
+        res.status(500).send('Произошла ошибка на сервере');
+        return;
+        }
+        // Отправляем содержимое файла как JSON
+        res.json(JSON.parse(data));
+    });
+});
 
 // Слушаем указанный порт
 app.listen(port, () => {
