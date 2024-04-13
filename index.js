9fc7521203c63a315691a6cdcceb8f7e5e81c97e
degradik <66140168+degradik@users.noreply.github.com>
2024-04-13 21:47:54 +0300

    +jsonget

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 // Маршрут обработки корневого URL
 app.get('/', (req, res) => {
-  res.send('Привет, мир!');
+    res.send('Привет, мир!');
 });
+
+app.get('/get', (req, res) => {
+    // Читаем файл example.json
+    fs.readFile('example.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Ошибка чтения файла:', err);
+        res.status(500).send('Произошла ошибка на сервере');
+        return;
+      }
+      // Отправляем содержимое файла как JSON
+      res.json(JSON.parse(data));
+    });
+  });
 
 // Слушаем указанный порт
 app.listen(port, () => {
