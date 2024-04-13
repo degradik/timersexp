6b597a3b54c1c2511f2cc40464f135057a3947f7
degradik <66140168+degradik@users.noreply.github.com>
2024-04-13 22:28:48 +0300

    +post

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 const port = 3000;
 
 // Маршрут обработки корневого URL
 app.get('/', (req, res) => {
-    res.send('Привет, мир!');
+    res.send('работает');
+});
+
+// Разбор JSON тела запроса
+app.use(bodyParser.json());
+
+// Маршрут для POST запроса
+app.post('/post', (req, res) => {
+    // Получаем данные из тела запроса
+    const jsonData = req.body;
+
+    // Преобразуем данные в JSON строку
+    const jsonString = JSON.stringify(jsonData);
+
+    // Путь к файлу, куда нужно сохранить данные
+    const filePath = "json/completed.json";
+
+    // Записываем данные в файл
+    fs.writeFile(filePath, jsonString, (err) => {
+        if (err) {
+            console.error('Ошибка при записи в файл:', err);
+            res.status(500).send('Произошла ошибка на сервере');
+            return;
+        }
+        console.log('Данные успешно сохранены в файл');
+        res.send('Данные успешно сохранены на сервере');
+    });
 });
 
 app.get('/get/completed', (req, res) => {
@@ -34,6 +63,7 @@ app.get('/get/inprocces', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+
 
 // Слушаем указанный порт
 app.listen(port, () => {
